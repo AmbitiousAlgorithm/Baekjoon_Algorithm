130aac282a753285ef136ce8641aa8afb828db56
HurJungMin <gjwjdlas@sookmyung.ac.kr>
2021-01-07 13:44:33 +0900

    [2021스터디] 기능개발 큐로 풀기

--- 프로그래머스/test.py
+++ 프로그래머스/test.py
@@ -1,1 +1,21 @@
-dev=[5,10,1,1,20,1]
+from collections import deque
+import math
+
+def solution(progresses, speeds):
+    answer = []
+    # 100 - progresses를 하고 speeds로 나눈 뒤 올림해준다.
+    progresses = list(map(lambda x,y: math.ceil((100-x)/y),progresses,speeds))
+    # 기준 값
+    release =  deque()
+    
+    for i in range(len(progresses)):
+        release.append(progresses[i])
+        if release[0] > progresses[i]:
+            answer.append(len(release))
+            release.clear()
+
+    answer.append(len(release))
+                  
+    return answer
+
+print(solution([93,30,55,50,89],[1,30,5,25,1]))
