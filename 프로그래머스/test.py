d18fe78d2db28ecaa704f1fb56a3f14114c16734
HurJungMin <gjwjdlas@sookmyung.ac.kr>
2021-01-14 21:02:09 +0900

    [2021 스터디] hash 나머지 한 점, 운송트럭, 스킬트리2

--- 프로그래머스/test.py
+++ 프로그래머스/test.py
@@ -1,3 +1,58 @@
-import bisect
-mylist = [1, 2, 3, 7, 9, 11, 33]
-print(bisect.bisect(mylist, 3))
+import heapq
+from collections import deque
+
+def solution(reqs):
+    answer = []
+    wait = []
+
+    reqs = deque([(-rank,time,idx) for idx,(rank,time) in enumerate(reqs,1)])
+    RANK, TIME, IDX = 0, 1, 2
+    heapq.heappush(wait, reqs.popleft())
+    
+    def nowait(wait):
+        if not wait and reqs:
+            heapq.heappush(wait, reqs.popleft())
+    time = 0
+
+    while wait:
+        running = heapq.heappop(wait)
+        end = running[TIME]
+        answer.append(running[IDX])
+        for _ in range(end):
+            time += 1 
+            if time % 3 == 0 and reqs:
+                heapq.heappush(wait, reqs.popleft())
+
+        nowait(wait)
+    return answer
+
+print(solution(	[[158, 2], [40, 5], [14, 4], [206, 5]]))
+
+
+# import heapq
+# from collections import deque
+
+# def solution(reqs):
+#     answer = []
+#     wait = []
+
+#     reqs = deque([(-rank,time,idx) for idx,(rank,time) in enumerate(reqs,1)])
+#     RANK, TIME, IDX = 0, 1, 2
+#     heapq.heappush(wait, reqs.popleft())
+    
+#     def nowait(wait):
+#         if not wait and reqs:
+#             heapq.heappush(wait, reqs.popleft())
+#     time = 0
+
+#     while wait:
+#         running = heapq.heappop(wait)
+#         end = running[TIME]
+#         answer.append(running[IDX])
+#         for _ in range(end):
+#             time += 1 
+#             if time % 3 == 0 and reqs:
+#                 heapq.heappush(wait, reqs.popleft())
+
+#         nowait(wait)
+#     return answer
