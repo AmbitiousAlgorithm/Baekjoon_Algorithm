2848c5153c05b0ae5c84f249173b68944d74a27a
HurJungMin <gjwjdlas@sookmyung.ac.kr>
2021-01-31 18:41:43 +0900

    [2021스터디] 정수삼각형

--- 프로그래머스/test2.py
+++ 프로그래머스/test2.py
@@ -1,34 +1,23 @@
+def solution(N, target):
+    
+    num_list = list({int(i*str(N))} for i in range(1,9))
+    
+    for i, number in enumerate(num_list, start = 1):
+        for j in range(i):
+            for x in num_list[j]:
+                for y in num_list[i-j-1]:
+                    num_list[i].add(x+y)
+                    num_list[i].add(x-y)
+                    num_list[i].add(x*y)
+                    if y != 0:
+                        num_list[i].add(x//y)
+        if target in num_list[i]:
+            return i+1
+    
+    return -1
 
-#  queue = deque([(infest)for infest in tuple(map(tuple,infests))])
-    
-#     visited = [[False]* n for _ in range(m)] # 지나간 자리 검사하는 배열
-#     DELTAS = ((0,1),(0,-1),(1,0),(-1,0)) 
-    
-#     def valid(x, y, m, n):
-#         return 0 <= x < m and 0 <= y < n
-    
-#     while queue:
-#         print(queue)
-#         x,y = queue.popleft()
-#         print(x,y)
-#         for dx, dy in DELTAS:
-#             next_x = x + dx
-#             next_y = y + dy
-            
-#             if valid(next_x, next_y, m, n) and [next_x, next_y] not in vaccinateds and queue:
-#                 print((next_x, next_y))
-#                 queue.append((next_x, next_y)) # 감염자 추가
-#                 visited[next_x][next_y] = True # 방문 체크
-#                 print(visited)
-                
-#     # for x in range(m):
-#     #     for y in range(n):
-#     #         visited[i][j] = True # 백신이면 그냥 true로 체크하고 넘어간다.
-#     #         bfs(m, n, x, y, infests, vaccinateds, visited)
-#     #         answer += 1
-
-            
-                
-#     # 처음부터 다 백신 다 감염인 경우
-#     if len(infests) == len(vaccinateds) == m == n:
-#         return 0
+if __name__ == '__main__':
+    N = 2
+    target = 11
+    result = solution(N, target)
+    print(result)
